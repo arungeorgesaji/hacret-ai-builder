7449c229e58ace873c2b8064563e59ecc4307218
arungeorgesaji <arungeorgesaji@gmail.com>
2025-06-26 18:28:29 +0530

    Fixed the run_test.sh(Ahh seems like I faced some issues mostly bcz with curl we need "ngrok-skip-browser-warning: true" and sometimes it didnt even show the warning when in browser)

--- app/main.py
+++ app/main.py
@@ -14,3 +14,7 @@ app.add_middleware(
 async def root():
     return {"status": "healthy"}
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
